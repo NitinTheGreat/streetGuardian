4b583a2381264689378d7a3dd1a406ab68216cf5
Nitin Kumar Pandey <83699589+NitinTheGreat@users.noreply.github.com>
2024-03-12 15:39:01 +0530

    Delete my_flask_app/dashboard.js

--- my_flask_app/dashboard.js
+++
@@ -1,6 +0,0 @@
-function toggleContent(header) {
-    const content = header.nextElementSibling;
-    content.classList.toggle('show');
-    const arrow = header.querySelector('.arrow');
-    arrow.textContent = content.classList.contains('show') ? '▲' : '▼';
-}
